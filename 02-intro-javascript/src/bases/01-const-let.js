e9abfc117ea526b974b99ebc5db557fe91e141a6
Martina Barea <martibarea01@gmail.com>
2023-04-06 15:33:36 -0300

    practica

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,14 @@
+// Variables y constantes 
+
+const nombre = 'Martina'
+const apellido = 'Barea'
+
+let valorDado = 6
+valorDado = 5
+
+console.log(nombre, apellido, valorDado)
+
+if (true) {
+    let valorDado = 5
+    console.log  (valorDado)
+}
